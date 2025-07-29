da18c8f1779badf01693d2bd805749b4608948fc
Samuel Heluany <samuelheluany@hotmail.com>
2025-07-29 10:31:38 -0300

    backend ok, create, update, delete users. Learn!

--- Api_users/server.js
+++ Api_users/server.js
@@ -1,19 +1,67 @@
 import express from 'express'
 
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 const app = express()
 
 app.use(express.json())
 
-const users = []
+// aqui insere os usuários no BD, precisa ser assincrona com await
+// cria o usuario com as informações do data
+app.post('/usuarios', async (req, res) => {
 
-app.post('/usuarios', (req, res) => {
+    // cria o usuário com as informações do data colocadas no POST
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
 
-    users.push(req.body)
-
+    // retorna o status 201 (deu certo) e o corpo da requisição como json
     res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req, res) => {
+// :id se torna uma variavel id
+app.put('/usuarios/:id', async (req, res) => {
+   
+    // editar um usuario pelo id (que é unico), no PUT coloca as
+    // informações novas que vão ser subsituidas quando der o GET
+    await prisma.user.update({
+         where: {
+        id: req.params.id
+    },
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
+
+    // resposta com status que deu certo
+    res.status(201).json(req.body)
+})
+
+// deletar usuario pelo id
+app.delete('/usuarios/:id', async (req, res) => {
+    // deletar pelo id
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    // mensagem de resposta que deletou
+    res.status(200).json({ message: 'Usuário deletado com sucesso!'})
+})
+
+app.get('/usuarios', async (req, res) => {
+    
+    const users = await prisma.user.findMany()
+
     res.status(200).json(users)
 })
 
